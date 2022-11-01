46834c62f821e3a5654ae5927d4d3d69799c8de2
I <henrikl@tuta.io>
2022-11-01 16:47:27 +0100

    unspecified

--- program/bash_scripts/update.sh
+++ program/bash_scripts/update.sh
@@ -2,5 +2,5 @@
 sleep 2
 git pull
 
-pkill -f "python3.10 tradfri_server.py"
-nohup python3.10 tradfri_server.py 192.168.0.171 & #TODO: remove hard-coding of ip
+pkill -f "python3 tradfri_server.py"
+nohup python3 tradfri_server.py 192.168.0.171 & #TODO: remove hard-coding of ip
