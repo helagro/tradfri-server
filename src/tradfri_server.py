6a1daf9c5b1ee79eaa77730d26f022dd4b6ad418
Henrik L <helagro@outlook.com>
2023-03-15 07:26:08 +0100

    unspecified

--- src/tradfri_server.py
+++ src/tradfri_server.py
@@ -13,19 +13,23 @@ VERSION = "1.11"
 def startServer():
     httpd = HTTPServer(('0.0.0.0', 8000), ReqHandler)
     httpd.serve_forever()
+
 def startServerThread():
     t = threading.Thread(target=startServer)
     t.start()
 
+
 def startSyncThread():
     t = threading.Thread(target=startSyncing)
     t.start()
+
 def startSyncing():
     sync_events.sync()
     schedule.every().day.at("01:00").do(sync_events.sync)
     while True: 
         time.sleep(30*60)
         schedule.run_pending()
+
 
 def startRoutinedThread():
     t = threading.Thread(target=schedule.start)
