81e166f020d448ac0634ea5e0695ccbab6ba3736
Henrik L <helagro@outlook.com>
2023-03-16 09:30:16 +0100

    unspecified

--- src/server/router.py
+++ src/server/router.py
@@ -11,20 +11,17 @@ def route(location: dict):
     command = location["c"][0]
     device = location["d"][0] if "d" in location else None
     payload = location["p"][0] if "p" in location else None
-
-    response = None
+    
 
     if(command == "devices"): 
-        response = tradfriInterface.getDevices()
+        return tradfriInterface.getDevices()
     elif(command == "logs"): 
-        response = logger.getLogs()
+        return logger.getLogs()
     elif(command == "update"): 
         subprocess.Popen("bash_scripts/update.sh")
         sys.exit()
     elif(command == "doEvent"): 
         event_schedule.performEventByName(payload)
     else: 
-        response = tradfriInterface.commandRouter(device, command, payload)
-
-    response
+        return tradfriInterface.commandRouter(device, command, payload)
 
