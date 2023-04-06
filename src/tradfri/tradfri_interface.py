df427d1d5cbac1349dccc046c971ec2ddac9bef9
Henrik L <helagro@outlook.com>
2023-04-06 14:34:00 +0200

    unspecified

--- src/tradfri/tradfri_interface.py
+++ src/tradfri/tradfri_interface.py
@@ -99,7 +99,7 @@ class TradfriInterface:
                 return self.commandRouterHelper(device, deviceID, "setBrightness", payload)
 
         elif command == "observe":
-            TradfriHandler.api(device.observe(observer))
+            TradfriHandler.api(TradfriHandler.api, device.observe(observer))
             logger.log("started observing", deviceID)
 
 
